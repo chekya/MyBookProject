8204e34d41ef6cf761ddacc45f326c06d7b3f0b9
yellowbox <yellowbox@naver.com>
2025-08-21 18:17:45 +0900

    8/21

---
+++ src/main/java/com/itgroup/bean/Book.java
@@ -0,0 +1,103 @@
+package com.itgroup.bean;
+
+public class Book {
+    private String title ;
+    private String author ;
+    private String publisher ;
+    private String publication_date ;
+    private int number_of_pages ;
+    private String category ;
+    private int price ;
+    private String ebook ;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", publication_date='" + publication_date + '\'' +
+                ", number_of_pages=" + number_of_pages +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", ebook='" + ebook + '\'' +
+                '}';
+    }
+
+    public Book(String title, String author, String publisher, String publication_date, int number_of_pages, String category, int price, String ebook) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.publication_date = publication_date;
+        this.number_of_pages = number_of_pages;
+        this.category = category;
+        this.price = price;
+        this.ebook = ebook;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublication_date() {
+        return publication_date;
+    }
+
+    public void setPublication_date(String publication_date) {
+        this.publication_date = publication_date;
+    }
+
+    public int getNumber_of_pages() {
+        return number_of_pages;
+    }
+
+    public void setNumber_of_pages(int number_of_pages) {
+        this.number_of_pages = number_of_pages;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getEbook() {
+        return ebook;
+    }
+
+    public void setEbook(String ebook) {
+        this.ebook = ebook;
+    }
+
+
+}
